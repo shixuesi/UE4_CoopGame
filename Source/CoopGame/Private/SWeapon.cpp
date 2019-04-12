957e6567ec615119bbdbd8bf27fb6f009891573e
shixuesi <shixuesi11@gmail.com>
2019-04-12 17:57:44 +0800

    添加开火初始功能

---
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "../Public/SWeapon.h"
+#include "DrawDebugHelpers.h"
+#include "Components/SkeletalMeshComponent.h"
+// Sets default values
+ASWeapon::ASWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComp"));
+	RootComponent = MeshComp;
+}
+
+// Called when the game starts or when spawned
+void ASWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASWeapon::Fire()
+{
+	AActor* MyOwner = GetOwner();
+
+	if (MyOwner)
+	{
+		FVector EyeLocation;
+		FRotator EyeRotation;
+
+		MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
+		FVector TraceEndPoint = EyeLocation + EyeRotation.Vector() * 1000;
+
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(MyOwner);
+		QueryParams.AddIgnoredActor(this);
+		QueryParams.bTraceComplex = true;
+
+		FHitResult Hit;
+		if (GetWorld()->LineTraceSingleByChannel(Hit, EyeLocation, TraceEndPoint,ECC_Visibility, QueryParams))
+		{
+			//…‰÷–ŒÔÃÂ
+		}
+
+		DrawDebugLine(GetWorld(), EyeLocation, TraceEndPoint, FColor::Red,false,1.0f,0,1.0f);
+	}
+}
+
+// Called every frame
+void ASWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
